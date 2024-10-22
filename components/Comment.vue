aa33b0f06f90d4b4044372080d0b48679f23a40c
Ksez0 <kubyshkin.ivan@yandex.ru>
2024-10-22 23:15:02 +0300

    add comment component

---
+++ components/Comment.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="comment">
+		<div class="comment__avatar"></div>
+		<div class="comment__content">
+			<div class="comment__header">
+				<span class="comment__username">{{ comment.user.fullName }}</span>
+			</div>
+			<p class="comment__text">{{ comment.body }}</p>
+			<div class="comment__footer">
+				<span class="comment__date">Today</span>
+				<span
+					class="comment__delete"
+					@click="handleDelete"
+					@mouseenter="isHovered = true"
+					@mouseleave="isHovered = false"
+				>
+					Удалить
+				</span>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Comment } from '~/types/Comment'
+
+const props = defineProps<{
+	comment: Comment
+}>()
+
+const emit = defineEmits<{
+	(e: 'delete', id: number): void
+}>()
+
+const isHovered = ref(false)
+
+const handleDelete = () => {
+	emit('delete', props.comment.id)
+}
+</script>
